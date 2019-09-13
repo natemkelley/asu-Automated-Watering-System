3ec14a8022e7c6c4eaf358840fae92ac574f1d62
nmkelley <natemkelley@gmail.com>
2019-09-13 12:09:29 -0700

    Themeing done

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Automated Watering System' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -28,14 +28,16 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
     '@nuxtjs/vuetify'
   ],
+  vuetify: {
+    optionsPath: '@/plugins/vuetify.options.js'
+  },
   /*
   ** Nuxt.js modules
   */
---
+++ plugins/vuetify.options.js
@@ -0,0 +1,16 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+    theme: {
+        themes: {
+          dark: {
+            primary: colors.white,
+            anchor: colors.red.darken1,
+          },
+          light: {
+            secondary: colors.yellow.darken1,
+            anchor: colors.yellow.darken1,
+          }
+        },
+      }
+  }
